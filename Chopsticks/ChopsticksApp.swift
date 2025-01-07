0f91cbab63dde2824e0509afe2460a90de3f4961
Musa Quddus <musa.q@berkeley.edu>
2025-01-06 23:45:53 -0800

    Initial Commit

---
+++ Chopsticks/ChopsticksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChopsticksApp.swift
+//  Chopsticks
+//
+//  Created by Musa Quddus on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ChopsticksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
